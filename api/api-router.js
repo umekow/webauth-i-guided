5e635a7f16460b14469df57d16905c09389104cc
umekow <umekolashun@gmail.com>
2019-11-11 11:36:12 -0600

    didn't finish

--- api/api-router.js
+++ api/api-router.js
@@ -10,4 +10,15 @@ router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
 
+router.post('/hash', (req, res) => {
+  const credentials = req.body; 
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash; 
+
+  if(!user || !bcrypt.compareSync(credentials.password, user.password)){
+    return res.status(401).json({error : 'Incorrect credentials'})
+  }
+
+})
+
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const apiRouter = require('./api-router.js');
+const configMiddleWare = require('./configure-middleware');
+
 
 const server = express();
+
+configMiddleWare(server); 
 
 server.use('/api', apiRouter);
 
